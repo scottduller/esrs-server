a5c25fbff9d625340e7f07a462eb0afd9a824914
scottduller <scottduller@gmail.com>
2021-05-02 18:12:51 +0100

    Bug fixing env variables

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(session({
-  secret: process.env.EXPRESS_SECRET,
+  secret: 'secret',
   resave: false,
   saveUninitialized: true,
 }));
