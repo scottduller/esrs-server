e18159cd99adb0e0abd3d63fabd0fb35e4697829
scottduller <scottduller@gmail.com>
2021-05-02 19:17:01 +0100

    Bug fixing env variables

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const session = require('express-session');
-const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('./config/passport');
 
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 
 require('./models/Level');
 require('./models/User');
@@ -46,37 +45,6 @@ if (!process.env.EXPRESS_SECRET) {
   console.log('EXPRESS_SECRET');
   process.exit(1);
 }
-
-const connectDB = async () => {
-  try {
-    let uri;
-
-    switch (process.env.NODE_ENV) {
-      case 'development':
-        uri = process.env.DEV_MONGO_URI;
-        break;
-      case 'test':
-        uri = process.env.TEST_MONGO_URI;
-        break;
-      case 'production':
-        uri = process.env.PROD_MONGO_URI;
-        break;
-      default:
-        throw new Error('Node environment invalid');
-    }
-
-    const conn = await mongoose.connect('mongodb+srv://admin:Password321@esrs-db.qj0cl.mongodb.net/testdb?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
-};
 
 connectDB();
 
